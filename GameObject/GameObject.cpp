4476575ce2b0fb2d5f20573c6f945a7871a72745
Magitrop <51723813+Magitrop@users.noreply.github.com>
2022-01-26 04:56:41 +0300

    Delete GameObject directory

--- GameObject/GameObject.cpp
+++
@@ -1,7 +0,0 @@
-#include "GameObject.h"
-
-GameObject::GameObject() : name("New Object")
-{
-	// every game object must contain irremovable transform component
-	AddComponent<TransformComponent>();
-}
